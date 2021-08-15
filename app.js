b329a8185b3563c457ed758b2704fd8ea49c2d30
Nithish06 <nithss0404@gmail.com>
2021-08-15 19:17:53 +0530

    Deleted

--- app.js
+++ app.js
@@ -26,26 +26,3 @@ function clickHandler() {
 }
 
 buttonTranslator.addEventListener("click", clickHandler);
-
-// var button = document.querySelector("#btn-click");
-// var input = document.querySelector("#text-input");
-// var translatedOutput = document.querySelector("#output");
-
-// var url = "https://api.funtranslations.com/translate/minion.json";
-
-// function getTranslatedUrl(text) {
-//   return url + "?" + "text=" + text;
-// }
-
-// button.addEventListener("click", clickHandler);
-
-// function clickHandler() {
-//   console.log("Clicked");
-//   var inputText = input.value;
-//   fetch(getTranslatedUrl(inputText))
-//     .then((response) => response.json())
-//     .then((json) => {
-//       var translatedOutputAnswer = json.contents.translated;
-//       translatedOutput.innerHTML = translatedOutputAnswer;
-//     });
-// }
